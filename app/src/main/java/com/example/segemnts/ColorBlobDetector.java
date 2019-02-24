bd7ba2450b92cb34c328b6248508aebb4b059d4b
kivicode <31818467+kivicode@users.noreply.github.com>
2019-02-24 16:24:22 +0300

    inital commit

--- app/src/main/java/com/example/segemnts/ColorBlobDetector.java
+++ app/src/main/java/com/example/segemnts/ColorBlobDetector.java
@@ -7,6 +7,7 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -41,16 +42,34 @@ class ColorBlobDetector {
     }
 
     void initDictionary() {
-        dictionary.put("1110111", 0);
-        dictionary.put("0010010", 1);
-        dictionary.put("1011110", 2);
-        dictionary.put("1011011", 3);
-        dictionary.put("0111010", 4);
-        dictionary.put("1101011", 5);
-        dictionary.put("1101111", 6);
-        dictionary.put("1010010", 7);
-        dictionary.put("1111111", 8);
-        dictionary.put("1111011", 9);
+        dictionary.put("7f", 0);
+        dictionary.put("77", 0);
+
+        dictionary.put("6f", 1);
+        dictionary.put("75", 1);
+        dictionary.put("67", 1);
+        dictionary.put("7d", 1);
+        dictionary.put("34", 1);
+        dictionary.put("35", 1);
+
+        dictionary.put("5f", 2);
+
+        dictionary.put("59", 3);
+        dictionary.put("5b", 3);
+
+        dictionary.put("7b", 4);
+        dictionary.put("39", 4);
+        dictionary.put("3b", 4);
+        dictionary.put("79", 4);
+
+        dictionary.put("6b", 5);
+
+        dictionary.put("4f", 6);
+
+        dictionary.put("4b", 7);
+
+//        dictionary.put("1111111", 8);
+//        dictionary.put("1111011", 9);
     }
 
     Mat getMask(int id, Mat orig, int x, int y, int w, int h) {
@@ -90,76 +109,55 @@ class ColorBlobDetector {
         return mask;
     }
 
-    Mat getScreen(Mat img) {
-        Mat gray = new Mat();
-        Imgproc.cvtColor(img, gray, Imgproc.COLOR_BGR2GRAY);
-        Mat blurred = new Mat();
-        Imgproc.GaussianBlur(gray, blurred, new Size(5, 5), 0);
-        Mat edged = new Mat();
-        Imgproc.Canny(blurred, edged, 50, 200, 255);
 
-        List<MatOfPoint> cnts = new ArrayList<>();
-        Mat H = new Mat();
-        Imgproc.findContours(edged, cnts, H, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-        MatOfPoint displayCnt = null;
+    Res process(Mat orig) {
+        try {
+            orig = extract(orig);
+        } catch (Exception e) {
 
-        for (Mat c : cnts) {
-            double peri = Imgproc.arcLength((MatOfPoint2f) c, true);
-            MatOfPoint2f approx = new MatOfPoint2f();
-            Imgproc.approxPolyDP(new MatOfPoint2f(((MatOfPoint) c).toArray()), approx, 0.02d * peri, true);
-            if (approx.total() == 4) {
-                displayCnt = (MatOfPoint) c;
-                break;
-            }
         }
-        List<MatOfPoint> cnt = new ArrayList<>();
-        cnt.add(displayCnt);
-        Imgproc.drawContours(img, cnt, -1, new Scalar(0, 0, 255), 4);
-        return edged;
-    }
 
-    Mat process(Mat orig) {
-//        orig = getScreen(orig);
 //        orig = imageProcessor(orig);
 //        Mat test = extract(orig);
         float exp = (float) 1.6;
-        int blur = 27;
-        int threshold = 87;
-        int adjustment = 5;
+        int blur = 29;
+        int threshold = 71;
+        int adjustment = 4;
         int erode = 3;
         int iterations = 3;
 
+        int w = 300;
+        int h = 150;
         Mat expose = new Mat();
         Scalar S = new Scalar(exp, exp, exp);
         Core.multiply(orig, S, expose);
 
-        int x = 100;
-        int y = 100;
-        int w = 100;
-        int h = 100;
-
-        expose = expose.submat(x, x + h, x, x + w);
+//            expose = expose.submat(x, x + h, x, x + w);
 
         Mat gray = new Mat();
         Imgproc.cvtColor(expose, gray, Imgproc.COLOR_BGR2GRAY);
 
         Mat cropped = new Mat();
-        Size s = new Size(blur, (int) 1.5 * blur);
+        Size s = new Size(blur, blur);
         Imgproc.blur(gray, cropped, s);
 
-        Mat cropped_threshold = new Mat();
-        Imgproc.adaptiveThreshold(cropped, cropped_threshold, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, threshold, adjustment);
-        Imgproc.cvtColor(cropped, cropped, Imgproc.COLOR_GRAY2BGR);
-
-        Mat karnel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(erode, erode));
         Mat inverse = new Mat();
-        Imgproc.erode(cropped_threshold, inverse, karnel, iterations);
-        Imgproc.GaussianBlur(inverse, inverse, new Size(9, 9), 0);
-//        Imgproc.cvtColor(inverse, inverse, Imgproc.COLOR_BGR2GRAY);
+        Mat cropped_threshold = new Mat();
+        Imgproc.adaptiveThreshold(cropped, inverse, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, threshold, adjustment);
+//                Imgproc.cvtColor(cropped, inverse, Imgproc.COLOR_GRAY2BGR);
+//                Imgproc.threshold(cropped, cropped_threshold, 0, 255, Imgproc.THRESH_BINARY_INV);
+//                Mat inverse = cropped_threshold;
+
+//                Mat karnel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(erode, erode));
+//                Mat inverse = new Mat();
+//                Imgproc.erode(cropped_threshold, inverse, karnel, iterations);
+//                Imgproc.blur(inverse, inverse, s);
+
 //
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hr = new Mat();
         Imgproc.findContours(inverse, contours, hr, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+
 
 //        float avgWidth = 0;
         float maxWidth = 0;
@@ -170,10 +168,11 @@ class ColorBlobDetector {
 //            avgArea += area;
 //        }
 //        avgArea /= contours.size();
+        Imgproc.drawContours(orig, contours, -1, new Scalar(10, 255, 70), 10);
         List<MatOfPoint> potetial_digits = new ArrayList<>();
         for (MatOfPoint contour : contours) {
             Rect sizes = Imgproc.boundingRect(contour);
-            if (sizes.width / sizes.height <= .7 && sizes.width * sizes.height > 900 && sizes.height > sizes.width) {
+            if (sizes.x < 400 && sizes.y < 300 && sizes.width / sizes.height <= .9 && sizes.width * sizes.height > 500 && sizes.height > sizes.width) {
                 potetial_digits.add(contour);
 //                avgWidth += sizes.width;
 //                avgHeight += sizes.height;
@@ -186,6 +185,8 @@ class ColorBlobDetector {
 //        avgWidth /= potentialdigits.size();
 //        avgHeight /= potentialdigits.size();
 
+        String TEXT = "";
+
         if (potetial_digits.size() > 0) {
             for (MatOfPoint cnt : potetial_digits) {
                 Rect sizes = Imgproc.boundingRect(cnt);
@@ -193,37 +194,43 @@ class ColorBlobDetector {
                     Point from = new Point(sizes.x + sizes.width, sizes.y);
                     Point to = new Point(sizes.x + sizes.width - (int) maxWidth, sizes.y + sizes.height);
                     Imgproc.rectangle(orig, from, to, new Scalar(255), 3);
-                    int[] segments = new int[7];
+                    String binaryStr = "";
                     for (int i = 0; i < 7; i++) {
                         try {
                             Mat mask = getMask(i, inverse, sizes.x, sizes.y, (int) maxWidth, (int) sizes.height);
                             Mat segment = new Mat();
-                            Core.bitwise_or(inverse, segment, mask);
+                            Core.bitwise_or(inverse, inverse, segment, mask);
                             float num = Core.countNonZero(segment);
                             float total = Core.countNonZero(mask);
                             float percent = (float) num / total;
-                            if (percent > 0.1) {
-                                segments[i] = 1;
+                            if (percent >= 0.35) {
+                                binaryStr += "1";
                             } else {
-                                segments[i] = 0;
+                                binaryStr += "0";
                             }
                         } catch (Exception ignored) {
 
                         }
                     }
-
-                    Log.d(TAG, Arrays.toString(segments));
+                    int decimal = Integer.parseInt(binaryStr, 2);
+                    String text = Integer.toString(decimal, 16);
+                    if (dictionary.containsKey(text)) {
+                        Imgproc.putText(orig, Integer.toString(dictionary.get(text)), new Point(sizes.x, sizes.y), Core.FONT_HERSHEY_COMPLEX_SMALL, 3, new Scalar(255, 0, 0), 3);
+                        TEXT += "|" + Integer.toString(dictionary.get(text));
+                    } else {
+                        Imgproc.putText(orig, text, new Point(sizes.x, sizes.y), Core.FONT_HERSHEY_COMPLEX_SMALL, 3, new Scalar(0, 0, 255), 3);
+                        TEXT += "(" + text + ")";
+                    }
                 }
             }
         }
-        w = 300;
-        h = 150;
-        Rect rect = new Rect((orig.width() - w) / 2, (orig.height() - h) / 2, w, h);
+//            return inverse;
 
-        Mat cropedMat = new Mat(orig, rect);
-        cropedMat.copyTo(orig.rowRange(0, cropedMat.rows()).colRange(0, cropedMat.cols()));
-        Imgproc.rectangle(orig, new Point((orig.width() - w) / 2, (orig.height() - h) / 2), new Point(w + (orig.width() - w) / 2, h + (orig.height() - h) / 2), new Scalar(0, 255, 0), 3);
-        return orig;
+
+//        Imgproc.rectangle(orig, new Point((orig.width() - w) / 2, (orig.height() - h) / 2), new Point(w + (orig.width() - w) / 2, h + (orig.height() - h) / 2), new Scalar(0, 255, 0), 3);
+//        Imgproc.cvtColor(inver, orig, Imgproc.COLOR_GRAY2BGR);
+        Res out = new Res(orig, TEXT);
+        return out;
     }
 
     Mat extract(Mat orig) {
@@ -232,12 +239,13 @@ class ColorBlobDetector {
         Mat blurred = new Mat();
         Imgproc.GaussianBlur(gray, blurred, new Size(5, 5), 0);
         Mat edged = new Mat();
-        Imgproc.Canny(blurred, edged, 100, 200, 3);
+        Imgproc.Canny(blurred, edged, 50, 200);
 
         List<MatOfPoint> cnts = new ArrayList<>();
         Mat H = new Mat();
         Imgproc.findContours(edged, cnts, H, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         MatOfPoint displayCnt = null;
+        List<MatOfPoint> pots = new ArrayList<>();
 
         for (MatOfPoint c : cnts) {
             MatOfPoint2f cont = new MatOfPoint2f(c.toArray());
@@ -247,96 +255,71 @@ class ColorBlobDetector {
 
             if (approx.total() == 4) {
                 displayCnt = new MatOfPoint(approx.toArray());
-                break;
+                pots.add(displayCnt);
             }
         }
-        cnts.clear();
-        cnts.add(displayCnt);
+        MatOfPoint bigggest = pots.get(0);
+        for (MatOfPoint dsp : pots) {
+            if (Imgproc.contourArea(dsp) > Imgproc.contourArea(bigggest)) {
+                bigggest = dsp;
+            }
+        }
 
-        Imgproc.drawContours(orig, cnts, -1, new Scalar(255, 0, 0), -1);
-
-//        Point sortedPoints[] = new Point[4];
-//
-//        MatOfPoint2f src = new MatOfPoint2f(
-//                sortedPoints[0],
-//                sortedPoints[1],
-//                sortedPoints[2],
-//                sortedPoints[3]);
-//
-//        MatOfPoint2f dst = new MatOfPoint2f(
-//                new Point(0, 0),
-//                new Point(450 - 1, 0),
-//                new Point(0, 450 - 1),
-//                new Point(450 - 1, 450 - 1)
-//        );
-//        Mat warpMat = Imgproc.getPerspectiveTransform(src,dst);
-//        //This is you new image as Mat
-//        Mat destImage = new Mat();
-//        Imgproc.warpPerspective(orig, destImage, warpMat, orig.size());
+        orig = correctPerspective(orig, bigggest);
+//        Imgproc.drawContours(orig, pots, -1, new Scalar(255, 0, 0), 7);
         return orig;
     }
 
-    public static Mat correctPerspective(Mat imgSource) {
-        Imgproc.Canny(imgSource.clone(), imgSource, 50, 50);
+    private static Mat correctPerspective(Mat sourceImage, MatOfPoint approx) {
 
-        // apply gaussian blur to smoothen lines of dots
-        Imgproc.GaussianBlur(imgSource, imgSource, new Size(5, 5), 5);
+        Moments moment = Imgproc.moments(approx);
+        int x = (int) (moment.get_m10() / moment.get_m00());
+        int y = (int) (moment.get_m01() / moment.get_m00());
 
-        // find the contours
-        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Imgproc.findContours(imgSource, contours, new
+//SORT POINTS RELATIVE TO CENTER OF MASS
+        Point[] sortedPoints = new Point[4];
 
-                Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-
-        double maxArea = -1;
-        MatOfPoint temp_contour = contours.get(0); // the largest is at the
-        // index 0 for starting
-        // point
-        MatOfPoint2f approxCurve = new MatOfPoint2f();
-
-        for (
-                int idx = 0; idx < contours.size(); idx++) {
-            temp_contour = contours.get(idx);
-            double contourarea = Imgproc.contourArea(temp_contour);
-            // compare this contour to the previous largest contour found
-            if (contourarea > maxArea) {
-                // check if this contour is a square
-                MatOfPoint2f new_mat = new MatOfPoint2f(temp_contour.toArray());
-                int contourSize = (int) temp_contour.total();
-                MatOfPoint2f approxCurve_temp = new MatOfPoint2f();
-                Imgproc.approxPolyDP(new_mat, approxCurve_temp, contourSize * 0.05, true);
-                if (approxCurve_temp.total() == 4) {
-                    maxArea = contourarea;
-                    approxCurve = approxCurve_temp;
-                }
+        double[] data;
+        int count = 0;
+        for (int i = 0; i < approx.rows(); i++) {
+            data = approx.get(i, 0);
+            double datax = data[0];
+            double datay = data[1];
+            if (datax < x && datay < y) {
+                sortedPoints[0] = new Point(datax, datay);
+                count++;
+            } else if (datax > x && datay < y) {
+                sortedPoints[1] = new Point(datax, datay);
+                count++;
+            } else if (datax < x && datay > y) {
+                sortedPoints[2] = new Point(datax, datay);
+                count++;
+            } else if (datax > x && datay > y) {
+                sortedPoints[3] = new Point(datax, datay);
+                count++;
             }
         }
+        MatOfPoint2f src = new MatOfPoint2f(
+                sortedPoints[0],
+                sortedPoints[1],
+                sortedPoints[2],
+                sortedPoints[3]);
 
-        Imgproc.cvtColor(imgSource, imgSource, Imgproc.COLOR_BayerBG2RGB);
-        Mat sourceImage = imgSource;
-        double[] temp_double;
-        temp_double = approxCurve.get(0, 0);
-        Point p1 = new Point(temp_double[0], temp_double[1]);
-        // Core.circle(imgSource,p1,55,new Scalar(0,0,255));
-        // Imgproc.warpAffine(sourceImage, dummy, rotImage,sourceImage.size());
-        temp_double = approxCurve.get(1, 0);
-        Point p2 = new Point(temp_double[0], temp_double[1]);
-        // Core.circle(imgSource,p2,150,new Scalar(255,255,255));
-        temp_double = approxCurve.get(2, 0);
-        Point p3 = new Point(temp_double[0], temp_double[1]);
-        // Core.circle(imgSource,p3,200,new Scalar(255,0,0));
-        temp_double = approxCurve.get(3, 0);
-        Point p4 = new Point(temp_double[0], temp_double[1]);
-        // Core.circle(imgSource,p4,100,new Scalar(0,0,255));
-        List<Point> source = new ArrayList<Point>();
-        source.add(p1);
-        source.add(p2);
-        source.add(p3);
-        source.add(p4);
-        Mat startM = Converters.vector_Point2f_to_Mat(source);
-        Mat result = warp(sourceImage, startM);
+        int w = 630;
+        int h = 230;
 
-        return result;
+        MatOfPoint2f dst = new MatOfPoint2f(
+                new Point(0, 0),
+                new Point(w - 1, 0),
+                new Point(0, h - 1),
+                new Point(w - 1, h - 1)
+        );
+        Mat warpMat = Imgproc.getPerspectiveTransform(src, dst);
+        //This is you new image as Mat
+        Mat destImage = new Mat();
+        Imgproc.warpPerspective(sourceImage, destImage, warpMat, sourceImage.size());
+        Imgproc.resize(destImage, destImage, sourceImage.size());
+        return destImage;
     }
 
     public static Mat warp(Mat inputMat, Mat startM) {
@@ -388,5 +371,29 @@ class ColorBlobDetector {
         Imgproc.cvtColor(temp, rgba, Imgproc.COLOR_GRAY2RGBA, 4);
 
         return temp;
+    }
+}
+
+class Res {
+    Mat mat;
+    String str;
+
+    Res(Mat m, String s) {
+        mat = m;
+        str = s;
+    }
+}
+
+class CustomComparator implements Comparator<MatOfPoint> {
+    @Override
+    public int compare(MatOfPoint o1, MatOfPoint o2) {
+        Rect rect1 = Imgproc.boundingRect(o1);
+        Rect rect2 = Imgproc.boundingRect(o2);
+        int result = 0;
+        double total = rect1.tl().y/rect2.tl().y;
+        if (total>=0.9 && total<=1.4 ){
+            result = Double.compare(rect1.tl().x, rect2.tl().x);
+        }
+        return result;
     }
 }
--- app/src/main/java/com/example/segemnts/MainActivity.java
+++ app/src/main/java/com/example/segemnts/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.segemnts;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -27,33 +31,56 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-//import android.view.View.OnTouchListener;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Reader;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.multi.GenericMultipleBarcodeReader;
+import com.google.zxing.multi.MultipleBarcodeReader;
+import com.google.zxing.oned.CodaBarReader;
+import com.google.zxing.qrcode.QRCodeReader;
+
 import android.view.View.OnClickListener;
 import android.view.SurfaceView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener, CvCameraViewListener2 {
-    private static final String  TAG              = "MainActivity";
+    private static final String TAG = "MainActivity";
 
-    private boolean              mIsColorSelected = false;
-    private Mat                  mRgba;
-    private ColorBlobDetector    mDetector;
+    private boolean mIsColorSelected = false;
+    private Mat mRgba;
+    private ColorBlobDetector mDetector;
+
+    private TextView contentTxt;
+    private TextView segmentTxt;
 
     private CameraBridgeViewBase mOpenCvCameraView;
 
-    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
+                case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
                     mOpenCvCameraView.enableView();
                     mOpenCvCameraView.setOnClickListener(MainActivity.this);
-                } break;
-                default:
-                {
+                }
+                break;
+                default: {
                     super.onManagerConnected(status);
-                } break;
+                }
+                break;
             }
         }
     };
@@ -70,6 +97,8 @@ public class MainActivity extends Activity implements OnClickListener, CvCameraV
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.activity_main);
+        contentTxt = (TextView) findViewById(R.id.scan_content);
+        segmentTxt = (TextView) findViewById(R.id.segmentTxt);
 
         mOpenCvCameraView = findViewById(R.id.color_blob_detection_activity_surface_view);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
@@ -77,16 +106,14 @@ public class MainActivity extends Activity implements OnClickListener, CvCameraV
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
@@ -113,22 +140,88 @@ public class MainActivity extends Activity implements OnClickListener, CvCameraV
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        System.gc();
         mRgba = inputFrame.rgba();
-
+        Mat mRgbaT = mRgba.t();
+        Core.flip(mRgba.t(), mRgbaT, 1);
+        Imgproc.resize(mRgbaT, mRgbaT, mRgba.size());
+        mRgba = mRgbaT;
+        Mat copy = new Mat();
+        mRgba.copyTo(copy);
         if (mIsColorSelected) {
+            final Res result = mDetector.process(mRgba);
+            mRgba = result.mat;
+            if(result.str != "") {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        segmentTxt.setText("7 SEGMENT CONTENT: " + result.str);
+                    }
+                });
+            }
             try {
-                mRgba = mDetector.process(mRgba);
-            }catch (Exception e){
-             e.printStackTrace();
+                zxing(copy);
+            } catch (ChecksumException e) {
+                e.printStackTrace();
+            } catch (FormatException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
         }
-
         return mRgba;
     }
 
 
     @Override
     public void onClick(View v) {
-        mIsColorSelected = !mIsColorSelected;
+        if (v.getId() == R.id.color_blob_detection_activity_surface_view) {
+            mIsColorSelected = !mIsColorSelected;
+        }
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+            contentTxt.setText("BARCODE CONTENT: " + scanContent);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public void zxing(Mat p) throws ChecksumException, FormatException {
+
+        Bitmap bMap = Bitmap.createBitmap(p.width(), p.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(p, bMap);
+        int[] intArray = new int[bMap.getWidth() * bMap.getHeight()];
+        //copy pixel data from the Bitmap into the 'intArray' array
+        bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+
+        LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(), intArray);
+
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        Reader reader = new MultiFormatReader();
+        String sResult = "";
+
+        try {
+
+            final Result result = reader.decode(bitmap);
+            runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    contentTxt.setText("BARCODE CONTENT: " + result.getText());
+                }
+            });
+
+        } catch (NotFoundException e) {
+            Log.d(TAG, "Code Not Found");
+            e.printStackTrace();
+        }
+
+    }
+
 }
